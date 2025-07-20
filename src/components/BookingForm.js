8c9ce1f5d2342f68a66feb2a7880b420a74aca10
njnfe <0xinniliu0@gmail.com>
2025-07-20 13:22:08 -0400

    Fix: import generateId and add generateId utility

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1527,3 +1527,11 @@ export default function BookingForm() {
     </div>
   );
 } 
+
+export function generateId() {
+  return (
+    'id_' +
+    Date.now().toString(36) +
+    Math.random().toString(36).substr(2, 9)
+  );
+}
